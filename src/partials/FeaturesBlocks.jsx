7829a280fa4aa4a5b1404f430f0e7b26d53e1be0
Anibe <samanibe@yahoo.com>
2023-03-07 04:02:59 +0100

    added update

--- src/partials/FeaturesBlocks.jsx
+++ src/partials/FeaturesBlocks.jsx
@@ -15,19 +15,26 @@ function FeaturesBlocks() {
           </div>
           <div className="lg:pr-10">
             <h5 className="mb-4 text-4xl font-extrabold leading-none">
-              Little Giants (“Ligi”) Foundation
+              Little Giants (“LiGi”) Foundation
             </h5>
             <p className="mb-6 text-gray-900">
               Our mission is to empower communities in Africa to access clean
               and safe water by providing technical assistance, resources, and
-              education. 
+              education.
             </p>
             <p className="mb-6 text-gray-900">
-            We strive to identify and address the root causes of
-              the water crisis and work with communities to establish
-              sustainable solutions. Our goal is to improve health, increase
-              economic opportunities, and enhance the overall quality of life
-              for the people and communities we serve.
+              We strive to identify and address the root causes of the water
+              crisis and work with communities to establish sustainable
+              solutions. Our goal is to improve health, increase economic
+              opportunities, and enhance the overall quality of life for the
+              people and communities we serve.
+            </p>
+
+            <p className="mb-6 text-gray-900">
+              Ligi’s sustainable projects provide support to communities, such
+              as through the construction of hand-dug and drilled wells,
+              pipelines, sand dams, rainwater harvesting systems and latrines,
+              and health and sanitation programmes
             </p>
           </div>
         </div>
