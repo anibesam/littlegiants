ec7e013b9ce2ea20fd27e3556ddbaf336ad9843b
Anibe <samanibe@yahoo.com>
2023-03-21 08:29:39 +0100

    fixed about

--- src/partials/AboutHero.jsx
+++ src/partials/AboutHero.jsx
@@ -14,12 +14,12 @@ function AboutHero() {
         </div>
       </div>
 
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="relative max-w-6xl mx-auto px-4 py-40">
         <div className="pt-32 md:pt-40">
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-16">
-            <h1 className="h1 font-playfair-display text-slate-100">About LIGI Foundation</h1>
+            <h1 style={{marginTop: "-100px"}} className="h1 font-playfair-display text-slate-100">About LIGI Foundation</h1>
             <p className='text-slate-100 max-w-2xl mt-3'>
             “Do the difficult things while they are easy and do the great things while they are small.
 A journey of a thousand miles must begin with a single step.
