406269235fbeb714f612b4c5856961f852e07831
Anibe <samanibe@yahoo.com>
2023-03-21 08:29:33 +0100

    fixed about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,7 +22,7 @@ function About() {
 
         {/*  Page sections */}
         <AboutHero />
-        <section className='mt-40'>
+        <section style={{marginTop: "-110px"}}>
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="grid gap-10 lg:grid-cols-2">
           <div>
